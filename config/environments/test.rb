ac7b187b87068874309c905d1376263c55b6e1dc
Johan Tique <johan.tique@analisistem.com>
2016-05-17 11:50:22 -0500

    remove digest on testing (warning)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.assets.digest = false
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
