93ccc1e47adba8ca1ff2b219d1466b4dae976b7e
Johan Tique <johan.tique@analisistem.com>
2016-05-17 11:38:58 -0500

    deal with heroku rules

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,21 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+###### Heroku deployment hack ######
+# # copy database config example file into 'real' file
+# if ENV['RACK_ENV'] == 'staging'
+#   config_root = File.join(Bundler.root, 'config')
+#   # PostgreSQL Config File
+#   postgresql_example_path = File.join(config_root, 'database.yml.example_postgresql')
+#   postgresql_real_path = File.join(config_root, 'database.yml')
+#   `cp #{postgresql_example_path} #{postgresql_real_path}`
+#   # MySQL Config File
+#   mysql_example_path = File.join(config_root, 'database.yml.example_mysql')
+#   mysql_real_path = File.join(config_root, 'database.yml')
+#   `cp #{mysql_example_path} #{mysql_real_path}`
+#   # Secrets File
+#   secrets_example_path = File.join(config_root, 'secrets.yml.example')
+#   secrets_real_path = File.join(config_root, 'secrets.yml')
+#   `cp #{secrets_example_path} #{secrets_real_path}`
+# end
